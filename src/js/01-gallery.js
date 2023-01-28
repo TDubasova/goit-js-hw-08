a0a05aae0f76de3711036436d2ec0c4ae78f8f83
Tetiana <tatyanadubasova1976@gmail.com>
2023-01-28 18:03:44 +0200

    project 8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,44 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+
+const pictureMarcup = creatingGalleryPictures(galleryItems);
+const galleryConteiner = document.querySelector(".gallery");
+galleryConteiner.insertAdjacentHTML("beforeend", pictureMarcup);
+
+// Відкриття модального вікна по кліку на елементі галереї та закриття по натисканню Escape
+galleryConteiner.addEventListener("click", onPictureClick);
+
+function creatingGalleryPictures(gallery) {
+  const markup = galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`
+    )
+    .join("");
+  return markup;
+}
+
+function onPictureClick(evt) {
+    evt.preventDefault();
+
+    const picture = evt.target.classList.contains("gallery__image");
+    if (!picture) {
+        return;
+    }
+}
+
+new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionSelector: "img",
+    captionsData: "alt",
+    captionDelay: 250,
+});
 
 console.log(galleryItems);
