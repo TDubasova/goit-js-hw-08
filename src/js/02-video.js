75a1553688040c470f6df733d6391ac698327e25
Tetiana <tatyanadubasova1976@gmail.com>
2023-01-29 18:33:18 +0200

    project 8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import Player from "@vimeo/player"
+import throttle from "lodash.throttle"
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const LOCALSTORAGE_KEY = "videoplayer-current-time"; 
+
+let data = {
+    duration: player.getDuration(),
+    persent: 0,
+    seconds: 0,
+}
+
+const  videoplayerCurrentTime = function ({ seconds }) {
+    localStorage.setItem(LOCALSTORAGE_KEY, seconds);
+}
+
+player.on('timeupdate', throttle(videoplayerCurrentTime, 1000));
+
+player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY)).then(function (seconds) {
+    seconds = videoplayerCurrentTime;
+});
+
 
